d6513966d8d091619e2705fcace580fea80eec93
Anton Tsarenko <itantontsarenko@gmail.com>
2025-12-01 19:22:51 +0200

    feat: Added timezone as a query parameter

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ To run the servlet on a local server, I used Apache Tomcat. Follow the steps bel
 1. Install Apache Tomcat on your local machine. You can download it from the [official website](https://tomcat.apache.org/download-90.cgi).
 2. Copy the generated WAR file (myname.war) from the build/libs directory to the webapps folder of your Tomcat installation.
 3. Start the Tomcat server by running the startup script located in the bin directory of your Tomcat installation.
-4. Open a web browser and navigate to http://localhost:8080/time/ to see the current UTC date and time
+4. Open a web browser and navigate to http://localhost:8080/time/ to see the current UTC date and time.
+5. If you need to know date and time in a different timezone, you can pass the timezone as a query parameter. 
+For example, to get the date and time in New York, use the URL: http://localhost:8080/time/?timezone=UTC+2
--- src/main/java/org/example/servlet/TimeServlet.java
+++ src/main/java/org/example/servlet/TimeServlet.java
@@ -11,11 +11,37 @@ import java.time.format.DateTimeFormatter;
 
 @WebServlet(value = "/")
 public class TimeServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
-        ZonedDateTime zdt = ZonedDateTime.now(ZoneId.of("UTC"));
+        String zoneParam = req.getParameter("timezone");
         resp.setContentType("text/html; charset=utf-8");
+        String zoneId;
+
+        // Default timezone
+        if (zoneParam == null || zoneParam.isBlank()) {
+            zoneId = "UTC";
+        } else {
+            String sign = zoneParam.contains("-") ? "-" : "+";
+            String time = zoneParam.substring(4);
+            String hours;
+            String minutes;
+            if (!time.contains(":")) {
+                hours = (time.length() == 1) ? "0" + time : time;
+                minutes = "00";
+            } else {
+                hours = (time.split(":")[0].length() == 1)
+                        ? "0" + time.split(":")[0]
+                        : time.split(":")[0];
+                minutes = (time.split(":")[1].length() == 1)
+                        ? "0" + time.split(":")[1]
+                        : time.split(":")[1];
+            }
+            zoneId = String.format("UTC%s%s:%s", sign, hours, minutes);
+        }
+
+        ZonedDateTime zdt = ZonedDateTime.now(ZoneId.of(zoneId));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
         resp.getWriter().write(zdt.format(formatter));
         resp.getWriter().close();
     }
